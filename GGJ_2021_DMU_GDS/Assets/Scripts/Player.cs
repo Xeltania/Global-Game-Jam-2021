72d5e7520ad25e9ca8aca5913ffe1ee62c4f243c
Jai Kothari <jai25@live.co.uk>
2021-01-30 02:03:48 +0000

    Added Test Player with basic movement script
    Will add functionality tomorrow

---
+++ GGJ_2021_DMU_GDS/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Variables.
+    [Header("Rigidbody Component")]
+    public Rigidbody rb;
+
+    [Header("Collider Component")]
+    public Collider col;
+
+    [Header("Movement Speed")]
+    public float speed;
+    public float acceleration;
+
+    public float curSpeed = 0;
+
+    //Start is called before the first frame update.
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    //Update is called once per frame.
+    void Update()
+    {
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector3(xMov, rb.velocity.y, zMov) * curSpeed * Time.deltaTime;
+
+        curSpeed = Mathf.MoveTowards(curSpeed, speed, acceleration);
+
+        if (curSpeed > speed)
+        {
+            curSpeed = speed;
+        }
+    }
+}
