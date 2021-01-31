c30026536339c49aeee0e6cb6b35ba695552c88f
Daniel Clipston <daniel_clipston@hotmail.com>
2021-01-31 12:45:15 +0000

    Added second method, not tested
    Neither methods of camera control have been tested, but both show a somewhat possible solution.
    Either go to spitscrreen mode, or back the camera up

---
+++ GGJ_2021_DMU_GDS/Assets/Scripts/CameraController2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController2 : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    private Camera mainCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player1.transform.position, player2.transform.position);
+        if(distance > 10) mainCam.transform.position = mainCam.transform.position - gameObject.transform.forward * distance;
+    }
+}
